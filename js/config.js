61656039b39dc6058af179a2328121179c32793c
I'm OWEN <146957570+oowenge@users.noreply.github.com>
2025-07-12 22:37:19 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -110,6 +110,18 @@ const API_SITES = {
         api: 'https://ikunzyapi.com/api.php/provide/vod',
         name: 'iKun资源'
     },
+    ikun2as: {
+        api: 'https://www.fantuan.tv/api.php/provide/vod/',
+        name: '饭团影视'
+    },
+    ikun: {
+        api: 'https://cj.lziapi.com/api.php/provide/vod/',
+        name: '影视工厂'
+    },
+    ikun: {
+        api: 'https://www.qiqidys.com/api.php/provide/vod/',
+        name: '七七资源'
+    },
     ikun1: {
         api: 'http://api.if101.tv/v1/vod',
         name: 'if101[屏蔽国内ip]'
